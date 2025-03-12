e4864ec7598c4ba6c71392a50f2b6fff0a90d0cb
thsdias <thdias.84@gmail.com>
2025-03-12 20:24:39 -0300

    Variaveis

---
+++ 02-variaveis.py
@@ -0,0 +1,20 @@
+
+print ('\n--------------------------------------------')
+print('Variaveis')
+print ('--------------------------------------------\n')
+
+num1 = 5
+num2 = 8.2
+soma = int(num1 + num2)
+print(f'\nA soma da conversão de inteiros de {num1} + {num2} é igual a {soma}.')
+print(f'A soma de {num1} + {num2} é igual a {num1 + num2}.')
+
+print('\n')
+
+var1 = 5
+var2 = '5'
+var3 = 5.0
+
+print(type(var1))
+print(type(var2))
+print(type(var3))
