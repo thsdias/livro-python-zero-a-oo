6ba70f272c79778a0727cfe00f392f8cd2a52f7d
thsdias <thdias.84@gmail.com>
2025-03-12 20:24:17 -0300

    Estrutura Basica

---
+++ 01-estrutura_basica.py
@@ -0,0 +1,8 @@
+
+nome1 = input('Digite o seu nome: ')
+nome2 = 'teste'
+
+print('Olá, ', nome1, '!')
+print('Seja bem-vindo(a) {} ao mundo Python!' .format(nome1))
+print('Seja bem-vindo(a) {} {} ao mundo Python!' .format(nome1, nome2))
+print(f'Seja bem-vindo(a) {nome1} {nome2} ao mundo Python!')
