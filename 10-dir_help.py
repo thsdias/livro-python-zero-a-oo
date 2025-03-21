705cd6060f8e112f4f5cd991974d314bf3f7858e
thsdias <thdias.84@gmail.com>
2025-03-21 20:13:55 -0300

    dir() e help()

---
+++ 10-dir_help.py
@@ -0,0 +1,20 @@
+
+print ('\n--------------------------------------------')
+print('dir() e help()')
+print ('--------------------------------------------\n')
+
+print('Usando Comando dir():')
+print('\n')
+
+list2 = [1, 'Carlor']
+print(dir(list2))   # Retorna todas as possiveis funcoes que podem ser executadas sobre uma lista.
+print('\n')
+
+
+print('\nUsando Comando help():')
+print('\n')
+
+print(help(list2))   # Retorna de forma mais detalhada, todas as possiveis funcoes que podem ser executadas sobre uma lista.
+
+
+print('\n')
